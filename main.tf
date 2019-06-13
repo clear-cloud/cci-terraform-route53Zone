88c188fb3d772a6756b841b0104369fe5fe59e4e
Chris Morgan <chris.morgan@clearcloud.co.uk>
2019-06-13 12:59:47 +0000

    init

---
+++ main.tf
@@ -0,0 +1,11 @@
+resource "aws_route53_zone" "vpc_public_zone" {
+  name = "${var.dns_domain}"
+
+  tags {
+    Name          = "${var.dns_domain}.${var.environment}.PublicZone"
+    Environment   = "${var.environment}"
+    Description   = "PublicZone"
+    Contact       = "${var.vpc_contact}"
+    Orchestration = "${var.global_orchestration}"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "global_orchestration" {}
+variable "vpc_contact" {}
+variable "dns_domain" {}
+variable "environment" {}
